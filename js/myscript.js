352b88cd1a259990feda2953c36bc14a232a0abe
Maisabdalrazeq <maisabdalrazq@gmail.com>
2020-08-26 12:51:01 +0300

    I added JS to My web page

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+
+
+var theowner = "mais";
+var gender = prompt("girl or boy!")
+alert('hello girls welcome to makeup world');
+alert(theowner);
+
+if(gender=girl){
+    alert("hello girls welcome to makeup world")
+} else {
+    alert("sorry try again")
+}
+
+console.log(heading);
